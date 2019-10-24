9452e6508680cbc8e1a24ff8ae9a842dddcbecf2
Nikita Furin <nokados@yandex.ru>
2019-10-24 21:32:17 +0300

    Drawing lines on the fly

--- fractals_strategy.lua
+++ fractals_strategy.lua
@@ -10,43 +10,37 @@ line = {{
 
 lines = {}
 out = {}
+fractalsId = 'fractals2'
+
 function Init()
-	local fractalsId = 'fractals2'
-	local numFracCandles = getNumCandles(fractalsId)
-	local priceUp, numLinesUp, nameUp = getCandlesByIndex(fractalsId, 1, 0, numFracCandles)
-	PrintDbgStr('UP: got '..numFracCandles..'(='..numLinesUp..') from '..nameUp)
-	PrintDbgStr(dump(priceUp))
-	local priceDown, numLinesDown, nameDown = getCandlesByIndex(fractalsId, 2, 0, numFracCandles)
-	PrintDbgStr('DOWN: got '..numFracCandles..'(='..numLinesDown..') from '..nameDown)
-	PrintDbgStr(dump(priceDown))
-
-    local b, e = nil, nil
-	for i = 0, #priceUp - 1 do
-		local el = priceUp[i]
-		if el.doesExist == 1 then
-			lines[#lines+1] = {i+1, el.high}
-		end
-	end
-
-	local li = 2 -- line index
-	for I = 1, numLinesUp do
-		out[I] = nil
-		if li <= #lines then
-			if I >= lines[li-1][1] then
-				out[I] = lines[li - 1][2] + ((lines[li][2] - lines[li - 1][2]) * (I - lines[li-1][1])) / (lines[li][1] - lines[li-1][1])
-			end
-			if lines[li][1] <= I then
-				li = li + 1
-			end
-		end
-	end
-
 	return #Settings.line
 end
 
+valId = nil
+val = nil
+function OnCalculate(index)
+	local priceUp, numLinesUp, nameUp = getCandlesByIndex(fractalsId, 1, index-1, 1)
+	if empty(priceUp) then
+		val = nil
+		return nil
+	end
+	el = priceUp[0]
+	if el.doesExist == 1 then
+		if val ~= nil then
+			drawLine(valId, val, index, el.high)
+		end
+		val = el.high
+		valId = index
+	end
+	return nil
+end
 
-function OnCalculate(Index)
-	return out[Index]
+function drawLine(indexStart, valStart, indexFinish, valFinish)
+	local stepReduce = (valFinish - valStart) / (indexFinish - indexStart)
+	for i = indexStart, indexFinish do
+		local stepId = i - indexStart
+		SetValue(i, 1, valStart + stepId * stepReduce)
+	end
 end
 
 function dump(o)
@@ -60,4 +54,9 @@ function dump(o)
    else
       return tostring(o)
    end
+end
+
+function empty(T)
+  for _ in pairs(T) do return false end
+  return true
 end
